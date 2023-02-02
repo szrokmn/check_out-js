36d738057e53ebe5c27465599c6df602a3cee204
Sezer Ökmen <szrokmn34@gmail.com>
2023-02-02 23:52:45 +0300

    productsDiv click event

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const taxRate = 0.18;
 const shippingPrice = 20;
 const shippingFreePrice = 300;
+
+const productsDiv = document.querySelector(".products");
+
+productsDiv.addEventListener("click", (event) => {
+    if (event.target.className == "fa-solid fa-minus") {
+        if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+            event.target.parentElement.querySelector(".quantity").innerText--;
+        } else {
+            if (
+                confirm(
+                    `${
+                        event.target.parentElement.parentElement.querySelector("h2").innerText
+                } will be deleted!!!`
+                )
+            ) {
+                event.target.closest(".product").remove();
+            }
+        }
+    } else if (event.target.className == "fa-solid fa-plus") {
+        event.target.previousElementSibling.innerText++;
+    } else if (event.target.className == "remove-product") {
+        // event.target.parentElement.parentElement.parentElement.remove()
+        event.target.closest(".product").remove()
+    }
+    calculateProductPrice(event.target);
+    calculateCartPrice();
+});
